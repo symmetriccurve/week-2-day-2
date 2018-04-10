0439f18a08222e0c238a1f90968d36f2edb4a4b0
Vikram Belde <belvikramm@gmail.com>
2018-04-10 15:45:35 -0500

    initial commit

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+require('./index.html')
+
+ReactDOM.render(
+   <h1>A simple react app with webpack-dev-servers that auto reloads on changes</h1>,
+  document.getElementById('root')
+);
